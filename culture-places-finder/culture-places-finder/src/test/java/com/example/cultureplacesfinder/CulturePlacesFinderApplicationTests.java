9a910f657f65e8f7ca3bb438de11dfbc7032d6ae
Mmark98 <58510020+Mmark98@users.noreply.github.com>
2025-03-19 20:17:40 +0100

    Add files via upload

---
+++ culture-places-finder/culture-places-finder/src/test/java/com/example/cultureplacesfinder/CulturePlacesFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cultureplacesfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturePlacesFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
